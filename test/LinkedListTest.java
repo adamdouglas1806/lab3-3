4fb9727c473ec6110f2b340ee310bc74cab2b8e4
adamd <adamd@Adams_Laptop.Home>
2025-10-08 13:26:41 +0100

    I have completed question 1. (Changes may be made after question 2).

--- test/LinkedListTest.java
+++ test/LinkedListTest.java
@@ -18,47 +18,95 @@ public class LinkedListTest {
 	@Test
 	public void testSizeEmpty() {
 		// test l.size() for an empty linked list
-		fail("not implemented yet");
+		assertEquals("The linked list is empty", 0, l.size());
+		//The assertEquals will check if l.size() is equal to 0, if it is then it is an empty linked list.
 	}
 
 	@Test
 	public void testSizeMany() {
 		// test l.size() after adding some numbers to the linked list
-		fail("not implemented yet");
+		l.addAtHead(1);
+		//l.addAtHead will add the integer 1 to the head of the linked list l.
+		l.addAtHead(2);
+		//l.addAtHead will add the integer 2 to the head of the linked list l.
+		l.addAtHead(3);
+		//l.addAtHead will add the integer 3 to the head of the linked list l.
+		
+		assertEquals("The sum of the integer nodes in the linked list", 3, l.size());
+		//The assertEquals will check if the number of nodes in the linked list is equal to 3.
 	}
 	
 	@Test
 	public void testSizeTwice() {
 		// test l.size() twice after adding some numbers to the linked list
-		fail("not implemented yet");
+		l.addAtHead(1);
+		//l.addAtHead will add the integer 1 to the head of the linked list l.
+		l.addAtHead(2);
+		//l.addAtHead will add the integer 2 to the head of the linked list l.
+		
+		assertEquals("The sum of the integer nodes in the linked list", 2, l.size());
+		//The assertEquals will check if the number of nodes in the linked list is equal to 2.
+		
+		l.addAtHead(3);
+		//l.addAtHead will add the integer 3 to the head of the linked list l.
+		
+		assertEquals("The sum of the integer nodes in the linked list", 3, l.size());
+		//The assertEquals will check if the number of nodes in the linked list is equal to 3.
 	}
 
 	@Test
 	public void testTotalEmpty() {
 		// test l.total() for an empty linked list
-		fail("not implemented yet");
+		assertEquals("The linked list is empty", 0, l.total());
+		//The assertEquals will check if l.total() is equal to 0, if it is then it is an empty linked list.
 	}
 
 	@Test
 	public void testTotalMany() {
 		// test l.total() after adding some numbers to the linked list
-		fail("not implemented yet");
+		l.addAtHead(1);
+		//l.addAtHead will add the integer 1 to the head of the linked list l.
+		l.addAtHead(2);
+		//l.addAtHead will add the integer 2 to the head of the linked list l.
+		l.addAtHead(3);
+		//l.addAtHead will add the integer 3 to the head of the linked list l.
+		
+		assertEquals("The sum of the integer nodes in the linked list", 6, l.size());
+		//The assertEquals will check the sum of all the integers in the linked list is equal to 6.
 	}
 	
 	@Test
 	public void testTotalTwice() {
 		// test l.total() twice after adding some numbers to the linked list
-		fail("not implemented yet");
+		l.addAtHead(1);
+		//l.addAtHead will add the integer 1 to the head of the linked list l.
+		l.addAtHead(2);
+		//l.addAtHead will add the integer 2 to the head of the linked list l.
+		
+		assertEquals("The sum of the integer nodes in the linked list", 3, l.size());
+		//The assertEquals will check the sum of all the integers in the linked list is equal to 3.
+		
+		l.addAtHead(3);
+		//l.addAtHead will add the integer 3 to the head of the linked list l.
+		
+		assertEquals("The sum of the integer nodes in the linked list", 6, l.size());
+		//The assertEquals will check the sum of all the integers in the linked list is equal to 6.
 	}
 	
 	@Test(expected=LinkedListException.class)
-	public void testRemoveAtHeadEmpty() {
+	public void testRemoveAtHeadEmpty() throws Exception {
 		l.removeAtHead();
+		
+		throw new Exception("LinkedListException");
+		//The throw new exception will prevent any errors from occurring when the user tries to remove from the head of a empty linked list.
 	}
 	
 	@Test(expected=LinkedListException.class)
-	public void testRemoveAtTailEmpty() {
+	public void testRemoveAtTailEmpty() throws Exception {
 		l.removeAtTail();
+		
+		throw new Exception("LinkedListException");
+		//The throw new exception will prevent any errors from occurring when the user tries to remove from the tail of a empty linked list.
 	}
 	
 	/*
